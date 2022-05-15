e843a643b75d6aceabe06503365517a35d17aae4
osho81 <ossi.hour@gmail.com>
2022-05-16 00:42:36 +0200

    Update output

--- src/main/java/databaseAction/DatabaseDelete.java
+++ src/main/java/databaseAction/DatabaseDelete.java
@@ -86,6 +86,10 @@ public class DatabaseDelete {
             transaction = entityManager.getTransaction();
             transaction.begin();
 
+            // Move out all disables/enables from all cases in switch
+//            Query disableConstraint = entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0");
+//            disableConstraint.executeUpdate(); // Disable constraint check
+
             System.out.println("Which table to empty/truncate?");
             System.out.println("1. Booking\n2. Customer\n3. Concert\n4. Arena\n5. Address\n6. All tables (Warning: all db content will be removed)");
             int userChoice = Main.validateUserIntegerChoice(6);
