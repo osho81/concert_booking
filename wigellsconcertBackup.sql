d027ef8859c2b67c9afc54ab38e6c9d7e06f4fcf
osho81 <ossi.hour@gmail.com>
2022-06-09 11:28:01 +0200

    Added database copy

---
+++ wigellsconcertBackup.sql
@@ -0,0 +1,168 @@
+CREATE DATABASE  IF NOT EXISTS `wigellsconcert` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `wigellsconcert`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: wigellsconcert
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `address` (
+  `address_id` int NOT NULL AUTO_INCREMENT,
+  `street` varchar(45) NOT NULL,
+  `house_no` varchar(45) NOT NULL,
+  `postalcode` varchar(45) NOT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`address_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address`
+--
+
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `arena`
+--
+
+DROP TABLE IF EXISTS `arena`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `arena` (
+  `arena_id` int NOT NULL AUTO_INCREMENT,
+  `arena_name` varchar(45) DEFAULT NULL,
+  `arena_address` int DEFAULT NULL,
+  `outside` set('yes','no') NOT NULL,
+  PRIMARY KEY (`arena_id`),
+  KEY `address_id_idx` (`arena_address`),
+  CONSTRAINT `fk_arena_address` FOREIGN KEY (`arena_address`) REFERENCES `address` (`address_id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `arena`
+--
+
+LOCK TABLES `arena` WRITE;
+/*!40000 ALTER TABLE `arena` DISABLE KEYS */;
+/*!40000 ALTER TABLE `arena` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `booking`
+--
+
+DROP TABLE IF EXISTS `booking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `booking` (
+  `booking_id` int NOT NULL AUTO_INCREMENT,
+  `concert` int DEFAULT NULL,
+  `customer` int DEFAULT NULL,
+  `number_of_tickets` int DEFAULT NULL,
+  PRIMARY KEY (`booking_id`),
+  KEY `fk_booked_concert_id_idx` (`concert`),
+  KEY `fk_booking_customer_id_idx` (`customer`),
+  CONSTRAINT `fk_booked_concert_id` FOREIGN KEY (`concert`) REFERENCES `concert` (`concert_id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `fk_booking_customer_id` FOREIGN KEY (`customer`) REFERENCES `customer` (`customer_id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking`
+--
+
+LOCK TABLES `booking` WRITE;
+/*!40000 ALTER TABLE `booking` DISABLE KEYS */;
+/*!40000 ALTER TABLE `booking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `concert`
+--
+
+DROP TABLE IF EXISTS `concert`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `concert` (
+  `concert_id` int NOT NULL AUTO_INCREMENT,
+  `artist` varchar(45) NOT NULL,
+  `concert_date` date NOT NULL,
+  `ticket_price` decimal(10,2) NOT NULL,
+  `concert_arena` int DEFAULT NULL,
+  PRIMARY KEY (`concert_id`),
+  KEY `fk_concert_arena_idx` (`concert_arena`),
+  CONSTRAINT `fk_concert_arena` FOREIGN KEY (`concert_arena`) REFERENCES `arena` (`arena_id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `concert`
+--
+
+LOCK TABLES `concert` WRITE;
+/*!40000 ALTER TABLE `concert` DISABLE KEYS */;
+/*!40000 ALTER TABLE `concert` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `birthday` date NOT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `customer_address` int DEFAULT NULL,
+  PRIMARY KEY (`customer_id`),
+  KEY `address_id_idx` (`customer_address`),
+  CONSTRAINT `fk_customer_address` FOREIGN KEY (`customer_address`) REFERENCES `address` (`address_id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-19 17:39:35
